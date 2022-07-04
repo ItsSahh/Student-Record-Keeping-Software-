03ca5627ed6cffee7255e6aa99f2929c5897c2c5
Armoghan <74015345+Armoghan-ul-Mohmin@users.noreply.github.com>
2022-07-05 01:28:27 +0500

    Add files via upload

--- ReadMe.md
+++ ReadMe.md
@@ -1,48 +1,66 @@
 # Student Record Keeping Software
 
 
-### Database <br>
-SRKS
 
-### Entity <br>
+
+### **Entity**
 
 1. Administartor: <br>
 
-            * Admin ID 
+            * Admin ID
             * Password
             * Admin Name
-            
+
 2. Student: <br>
 
             * Student ID
             * Student Name
             * Address
             * Contact No
-            
+
 3. Fees: <br>
 
             * Name
             * Recipent No
             * Tution Fee
-            
+
 4. Department: <br>
 
             * Department ID
             * Department Name
-           
+
 5. Course: <br>
 
             * Course ID
             * Course Name
-            
+
 6. Exame: <br>
 
             * Reg No
             * Course
             * Subject Name
             * Marks
-            
-7. Teacher: <br>
+
+7. Teacher:
 
             * Teacher ID
             * Teacher Name
+
+
+ ### **Dumping Values**
+
+     Administartor Table
+
+    | Admin_ID | Admin_Name          | Admin_Password       |
+    | -------- | ------------------- |-------------------
+    |   1      |   Root              |    Root_Password     |
+    |   2      |   Admin             |    Admin_Password    |
+    |   3      |   Student           |    Student_Password  |
+
+    Student Table
+
+    | Student_ID | Student_Name      | Address         |  Contact          |
+    | -------- | ------------------- |----------------- -------------------
+    |   1      |   Armoghan          |    Lahore       | 03328695097       |
+    |   2      |   Aftab Shah        |    Lahore       | 0301 6589918      |
+    
--- Student Record Keeping Software.sql
+++ Student Record Keeping Software.sql
@@ -6,20 +6,20 @@ ________________________________________________________________________________
 -- Table Structure for  Administartor Table.
 
 Create TABLE Administartor(
-    Admin_ID    BIGINT Not Null ,
-    Admin_Name  VARCHAR(20) Not Null,
-    Admin_Password    VARCHAR(20) Not Null
+    Admin_ID            BIGINT Not Null ,
+    Admin_Name          VARCHAR(20) Not Null,
+    Admin_Password      VARCHAR(20) Not Null
 );
 ________________________________________________________________________________
 
 -- Table Structure for  Student Table.
 
 Create TABLE Student(
-    Student_ID   BIGINT Not Null ,
-    Student_Name VARCHAR(20) Not Null,
-    Student_Roll-No BIGINT Not Null,
-    Student_Address VARCHAR(100) Not Null,
-    Contact_No  VARCHAR(100) Not Null
+    Student_ID          BIGINT Not Null ,
+    Student_Name        VARCHAR(20) Not Null,
+    Student_Roll-No     BIGINT Not Null,
+    Student_Address     VARCHAR(100) Not Null,
+    Contact_No          VARCHAR(100) Not Null
 );
 ________________________________________________________________________________
 
@@ -27,43 +27,43 @@ ________________________________________________________________________________
 -- Table Structure for Fee Table.
 
 Create TABLE Fee(
-    Student_Name VARCHAR(20) Not Null,
-    Recipent_No BIGINT Not Null,
-    Tution_Fee BIGINT Not Null
+    Student_Name        VARCHAR(20) Not Null,
+    Recipent_No         BIGINT Not Null,
+    Tution_Fee          BIGINT Not Null
 );
 ________________________________________________________________________________
 
 -- Table Structure for Department Table
 
 Create TABLE Department(
-    Department_ID   BIGINT Not Null ,
-    Department_Name VARCHAR(20) Not Null
+    Department_ID       BIGINT Not Null ,
+    Department_Name     VARCHAR(20) Not Null
 );
 ________________________________________________________________________________
 
 -- Table Structure for Course Table
 
 Create TABLE Coures(
-    Course_ID   BIGINT Not Null ,
-    Coures_Name VARCHAR(20) Not Null
+    Course_ID           BIGINT Not Null ,
+    Coures_Name         VARCHAR(20) Not Null
 );
 ________________________________________________________________________________
 
 --Table Structure for Exame Table
 
 Create TABLE Exame(
-    Reg_No  TINYINT ,
-    Course VARCHAR(20),
-    Subject VARCHAR(20),
-    Marks   TINYINT
+    Reg_No              BIGINT ,
+    Course              VARCHAR(20),
+    Subject             VARCHAR(20),
+    Marks               BIGINT
 );
 ________________________________________________________________________________
 
 --Table Structure for Teacher Table
 
 Create TABLE Teacher(
-    Teacher_ID  BIGINT Not Null ,
-    Teacher_Name VARCHAR(20) Not Null
+    Teacher_ID          BIGINT Not Null ,
+    Teacher_Name        VARCHAR(20) Not Null
 );
 ________________________________________________________________________________
 
